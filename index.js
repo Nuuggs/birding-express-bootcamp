ea221b7ebcb4847a10abfa0b509b2aaf87f7a53b
Nuuggs <bryluke000@gmail.com>
2021-11-16 22:30:47 +0800

    completed comfortable

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-import express from 'express';
+import express, { response } from 'express';
 import pg from 'pg';
-
-// Initialise DB connection
-const { Pool } = pg;
-const pgConnectionConfigs = {
-  user: 'blt',
-  host: 'localhost',
-  database: 'birding',
-  port: 5432,
-};
-const pool = new Pool(pgConnectionConfigs);
+import methodOverride from 'method-override';
 
 // Initialise express
 const app = express();
@@ -17,9 +8,19 @@ const app = express();
 // Set view engine
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+// Initialise DB connection
+const { Pool } = pg;
+const pgConnectionConfigs = {
+  user: 'bryluke000',
+  host: 'localhost',
+  database: 'birding',
+  port: 5432,
+};
+const pool = new Pool(pgConnectionConfigs);
 
 // Do stuff here:
-
 app.get('/note', (req, res) => {
   console.log('get request came in, "/note"');
 
@@ -33,7 +34,7 @@ app.post('/note', (req, res) => {
     inputFormObj.date,
     inputFormObj.appearance,
     inputFormObj.behaviour,
-    inputFormObj.vocalisations,
+    inputFormObj.vocalisation,
     inputFormObj.flock_size,
   ];
 
@@ -74,9 +75,59 @@ app.get('/', (req, res) => {
     if (error) {
       console.log('select all error', error);
     }
+    console.log(result.rows);
     const ejsObject = { notes: result.rows };
 
     res.render('index', ejsObject);
+  });
+});
+
+app.get('/note/:id/edit', (req, res) => {
+  console.log('request came in, "/note/:id/edit"')
+  const databaseId = Number(req.params.id) + 1;
+  const sqlQuery = `SELECT * FROM notes WHERE id=${databaseId}`;
+
+  pool.query(sqlQuery, (error, result) => {
+    if (error) {
+      console.log('select id error', error);
+    }
+
+    console.log(result.rows[0]);
+    const ejsObject = result.rows[0];
+    ejsObject.index = req.params.id;
+    res.render('edit', ejsObject);
+  });
+  
+});
+
+app.put('/note/:id', (req, res) => {
+  console.log('put request coming in, "/note/:id"');
+
+  const databaseId = Number(req.params.id) + 1;
+  const inputFormObj = req.body;
+
+  const inputArr = [
+    inputFormObj.habitat,
+    inputFormObj.date,
+    inputFormObj.appearance,
+    inputFormObj.behaviour,
+    inputFormObj.vocalisation,
+    inputFormObj.flock_size,
+  ];
+
+  console.log(inputArr);
+
+  const sqlQuery = `UPDATE notes 
+  SET habitat = $1, date = $2, appearance = $3, behaviour = $4, vocalisation = $5, flock_size = $6
+  WHERE id=${databaseId} RETURNING *`;
+
+  pool.query(sqlQuery, inputArr, (error, result) => {
+    if (error) {
+      console.log('error', error);
+    }
+
+    console.log(result.rows);
+    res.redirect(`/note/${req.params.id}`);
   });
 });
 
